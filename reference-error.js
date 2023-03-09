290a72e7710f1a7bf93889f12f1d3ba0099c3b8a
shinjons99 <shinjons99@gmail.com>
2023-03-09 13:45:23 +0600

    Understand Reference errors and Type errors messages

---
+++ reference-error.js
@@ -0,0 +1,20 @@
+const x = 'pori';
+const y = 'moni';
+console.log(z);
+
+console.log(name);
+const name = 'Ananta jalil'
+
+console.log(add(2, 3));
+function add(n1, n2){
+    return n1 + n2;
+}
+
+const add2 = (n1, n2) => n1+n2;
+
+function doSomething(){
+    const sum = 55;
+    console.log(
+        sum
+    );
+}
---
+++ type-error.js
@@ -0,0 +1,7 @@
+const num = 5;
+// num.push(45);//*wrong type that's called type error
+
+// let address;
+// console.log(address.home);
+let address={ city: 'bBaria', postCode: 4560};
+console.log(address?.home.building);
